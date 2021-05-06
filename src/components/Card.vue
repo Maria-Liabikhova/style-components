3942a57d872cc31e67bd01565b780747b2cf33c6
apospeliri <apospeliri@gmail.com>
2021-05-06 16:31:26 +0300

    add - styled component cssModificator + active class

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,6 +4,8 @@
       <h1 :class="cssModificator.title" class="card__title">Hello World!</h1>
       <img :src="require('@/assets/img/colourfill.jpg')" alt="colourfull friends" :class="cssModificator.img" class="card__img">
     </div>
+    <div :class="[cssModificator.btn, {active: isCollase}]" class="btn" @click="isCollase = !isCollase">button with 2 active styles</div>
+
     <p :class="cssModificator.text" class="card__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quo ipsam quae, dolore corrupti aut sunt ducimus quod repellat sint blanditiis maxime suscipit. Qui libero, inventore dolores odit mollitia fuga.</p>
     <slot></slot>
   </div>
@@ -19,13 +21,14 @@ export default {
       default: () => ({
         title: '',
         text: '',
-        img: ''
+        img: '',
+        btn: ''
       })
     }
   },
   data() {
     return {
-
+      isCollase: false
     }
   }
 }
--- src/pages/Second.vue
+++ src/pages/Second.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <card class="card--violet" :cssModificator="{title: 'card__title--green', text: 'card__text--orange'}">
+    <card class="card--violet" :cssModificator="{title: 'card__title--green', text: 'card__text--orange', btn: 'btn--green'}">
       <router-link to="/" class="card__link">Another page</router-link> 
     </card>
     <btn class="btn--green"></btn>
