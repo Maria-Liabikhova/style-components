1c7882a7a57a19d7dec8b33423e4be28d0fdc998
apospeliri <apospeliri@gmail.com>
2021-05-06 16:17:46 +0300

    add - styled component cssModificator

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="card">
     <div class="card__flexwrap">
-      <h1 :class="titleClass" class="card__title">Hello World!</h1>
-      <img :src="require('@/assets/img/colourfill.jpg')" alt="colourfull friends" class="card__img">
+      <h1 :class="cssModificator.title" class="card__title">Hello World!</h1>
+      <img :src="require('@/assets/img/colourfill.jpg')" alt="colourfull friends" :class="cssModificator.img" class="card__img">
     </div>
-    <p :class="textClass" class="card__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quo ipsam quae, dolore corrupti aut sunt ducimus quod repellat sint blanditiis maxime suscipit. Qui libero, inventore dolores odit mollitia fuga.</p>
+    <p :class="cssModificator.text" class="card__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quo ipsam quae, dolore corrupti aut sunt ducimus quod repellat sint blanditiis maxime suscipit. Qui libero, inventore dolores odit mollitia fuga.</p>
     <slot></slot>
   </div>
 </template>
@@ -13,15 +13,14 @@
 export default {
   name: "Card",
   props: {
-    titleClass: {
-      type: String,
+    cssModificator: {
+      type: Object,
       required: false,
-      default: ''
-    },
-    textClass: {
-      type: String,
-      required: false,
-      default: ''
+      default: () => ({
+        title: '',
+        text: '',
+        img: ''
+      })
     }
   },
   data() {
--- src/pages/Second.vue
+++ src/pages/Second.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <card class="card--violet" :titleClass="'card__title--green'" :textClass="'card__text--orange'"><router-link to="/" class="card__link">Another page</router-link> </card>
+    <card class="card--violet" :cssModificator="{title: 'card__title--green', text: 'card__text--orange'}">
+      <router-link to="/" class="card__link">Another page</router-link> 
+    </card>
     <btn class="btn--green"></btn>
   </div>
 </template>
 
 <script>
-import card  from '../components/Card.vue'
-import btn  from '../components/Btn.vue'
+import card from "../components/Card.vue";
+import btn from "../components/Btn.vue";
 export default {
-  components:  {card, btn},
+  components: { card, btn },
   data() {
     return {
-      isGreen: true
-    }
-  }
-}
+      isGreen: true,
+    };
+  },
+};
 </script>
